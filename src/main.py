7a8c11b53abf14b12e560ffe75638bdd3257ab22
danielbukhosi <danieltshuma64@gmail.com>
2026-02-25 12:20:32 +0100

    wrapped imported functions inside a main function

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 import load_to_db #import main as LoadToDb
 import create_views #import main as CreateViews
 import unload_results #import main as LoadToJson
-load_to_db.main()
-create_views.main()
-unload_results.main()
+
+def main():
+  load_to_db.main()
+  create_views.main()
+  unload_results.main()
+
+if __name__ == "__main__":
+  main()
