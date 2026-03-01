bee9d653958d10ef42ba44a3b6b65e39ce435e54
danielbukhosi <danieltshuma64@gmail.com>
2026-03-01 18:30:08 +0100

    modified __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,20 @@
 from .load_to_db import Rooms, Students
-from .create_views import NumberOfStudentsInRoomsView,RoomsWithDiffSexStudentsView,RoomsWithLargestAgeDiffView,RoomsWithSmallestAvgAgeView, main
-from .unload_results import PostgresClient,DataExporter,RoomAnalyticsService, main
+from .create_views import (
+    NumberOfStudentsInRoomsView,
+    RoomsWithDiffSexStudentsView,
+    RoomsWithLargestAgeDiffView,
+    RoomsWithSmallestAvgAgeView,
+    main as create_views_main
+)
+from .unload_results import PostgresClient, DataExporter, RoomAnalyticsService, main as unload_results_main
 
-__all__ = ["Rooms","Students","NumberOfStudentsInRoomsView",
-           "RoomsWithDiffSexStudentsView","RoomsWithLargestAgeDiffView",
-           "RoomsWithSmallestAvgAgeView","mLoad"]
+__all__ = [
+    "Rooms", "Students",
+    "NumberOfStudentsInRoomsView",
+    "RoomsWithDiffSexStudentsView",
+    "RoomsWithLargestAgeDiffView",
+    "RoomsWithSmallestAvgAgeView",
+    "create_views_main",
+    "unload_results_main",
+    "PostgresClient", "DataExporter", "RoomAnalyticsService"
+]
