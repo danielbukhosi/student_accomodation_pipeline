ba5b02d0cbda7e09555db30a423d4e3b5615e7a8
danielbukhosi <danieltshuma64@gmail.com>
2026-03-01 17:50:04 +0100

    added unload_results new classes to init.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,6 @@
 from .load_to_db import Rooms, Students
 from .create_views import NumberOfStudentsInRoomsView,RoomsWithDiffSexStudentsView,RoomsWithLargestAgeDiffView,RoomsWithSmallestAvgAgeView, main
-from .unload_results import NumberOfStudentsInRoomsJ,RoomsWithDiffSexStudentsJ,RoomsWithLargestAgeDiffJ,RoomsWithSmallestAvgAgeJ, main
-from .unload_results import NumberOfStudentsInRoomsX,RoomsWithDiffSexStudentsX,RoomsWithLargestAgeDiffX,RoomsWithSmallestAvgAgeX, main
+from .unload_results import PostgresClient,DataExporter,RoomAnalyticsService, main
 
 __all__ = ["Rooms","Students","NumberOfStudentsInRoomsView",
            "RoomsWithDiffSexStudentsView","RoomsWithLargestAgeDiffView",
