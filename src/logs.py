aaef142c39b5f3893b4bff8ed0efd4fe69ed1ead
danielbukhosi <danieltshuma64@gmail.com>
2026-03-03 11:20:57 +0100

    remove duplicate functions and resorted to one

--- src/logs.py
+++ src/logs.py
@@ -1,8 +1,8 @@
 import logging
 import sys
 
-def load_to_db_log():
-  logger = logging.getLogger("LoadToDb")
+def get_logger():
+  logger = logging.getLogger("__main__")
   handler = logging.StreamHandler(sys.stdout)
   formatter = logging.Formatter("%(asctime)s-%(name)s-%(levelname)s-%(message)s")
   handler.setFormatter(formatter)
@@ -10,28 +10,10 @@ def load_to_db_log():
   logger.setLevel(logging.DEBUG)
   return logger
 
-def create_views_log():
-  logger =  logging.getLogger("CreateViews")
-  handler = logging.StreamHandler(sys.stdout)
-  formatter = logging.Formatter("%(asctime)s-%(name)s-%(levelname)s-%(message)s")
-  handler.setFormatter(formatter)
-  logger.addHandler(handler)
-  logger.setLevel(logging.DEBUG)
-  return logger
 
-def unload_results_log():
-  logger = logging.getLogger("UnloadToDb")
-  handler = logging.StreamHandler(sys.stdout)
-  formatter = logging.Formatter("%(asctime)s-%(name)s-%(levelname)s-%(message)s")
-  handler.setFormatter(formatter)
-  logger.addHandler(handler)
-  logger.setLevel(logging.DEBUG)
-  return logger
 
 def main():
-  logger_1 = load_to_db_log()
-  logger_2 = create_views_log()
-  logger_3 = unload_results_log()
+  logger = get_logger()
 
 if __name__ == "__main__":
   main()
