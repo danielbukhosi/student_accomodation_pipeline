dfce3dacdbd6e842d0b63e1e169cf6792900989e
danielbukhosi <danieltshuma64@gmail.com>
2026-03-03 11:30:47 +0100

    added get_logger to init.py

--- src/__init__.py
+++ src/__init__.py
@@ -7,7 +7,7 @@ from .create_views import (
     main as create_views_main
 )
 from .unload_results import PostgresClient, DataExporter, RoomAnalyticsService, main as unload_results_main
-from .logs import unload_results_log,create_views_log, load_to_db_log
+from .logs import get_logger, main as get_logger_main
 
 __all__ = [
     "Rooms", "Students",
@@ -18,5 +18,5 @@ __all__ = [
     "create_views_main",
     "unload_results_main",
     "PostgresClient", "DataExporter", "RoomAnalyticsService",
-    "unload_results_log","create_views_log","load_to_db_logs"
+    "get_logger", "get_logger_main"
 ]
